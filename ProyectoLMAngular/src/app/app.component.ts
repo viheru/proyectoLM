3d8541658862a7ac3ad3c240d6fd427cf0c9c15a
Vicente <Vicente@gmail.com>
2025-03-30 11:28:52 +0200

    añadir footer y otros cambios

--- ProyectoLMAngular/src/app/app.component.ts
+++ ProyectoLMAngular/src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./componentes/navbar/navbar.component";
-import { InicioComponent } from "./componentes/inicio/inicio.component";
-import { CatalogoComponent } from "./componentes/catalogo/catalogo.component";
-import { ProductoComponent } from "./componentes/producto/producto.component";
-import { PagoComponent } from "./componentes/pago/pago.component";
-import { ContactoComponent } from "./componentes/contacto/contacto.component";
-import { IniciosesionComponent } from "./componentes/iniciosesion/iniciosesion.component";
-import { RegistroComponent } from "./componentes/registro/registro.component";
 import { FooterComponent } from "./componentes/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, InicioComponent, CatalogoComponent, ProductoComponent, PagoComponent, ContactoComponent, IniciosesionComponent, RegistroComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ProyectoLMAngular/src/app/app.routes.ts
+++ ProyectoLMAngular/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { CatalogoComponent } from './componentes/catalogo/catalogo.component';
+import { ProductoComponent } from './componentes/producto/producto.component';
+import { PagoComponent } from './componentes/pago/pago.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { IniciosesionComponent } from './componentes/iniciosesion/iniciosesion.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'inicio', component: InicioComponent},
+    {path: 'navbar', component: NavbarComponent},
+    {path: 'footer', component: FooterComponent},
+    {path: 'catalogo', component: CatalogoComponent},
+    {path: 'producto', component: ProductoComponent},
+    {path: 'pago', component: PagoComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: 'iniciosesion', component: IniciosesionComponent},
+    {path: 'registro', component: RegistroComponent},
+    {path: '', redirectTo: '/inicio', pathMatch: 'full'}
+
+];
