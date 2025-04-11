bb8096afe7d42bf61552059f11149dc0323f8a73
Vicente <Vicente@gmail.com>
2025-04-11 11:09:35 +0200

    cambios en data.json y en el archivo catalogo

--- ProyectoLMAngular/src/app/common/catalogo.ts
+++ ProyectoLMAngular/src/app/common/catalogo.ts
@@ -1,8 +1,13 @@
 export interface Catalogo {
+    nombreTienda: string
+    productos: Producto[]
+  }
+  
+  export interface Producto {
     id: number
     nombre: string
     descripcion: string
     precio: number
     imagen: string
-}
+  }
 
--- ProyectoLMAngular/src/app/componentes/catalogo/catalogo.component.ts
+++ ProyectoLMAngular/src/app/componentes/catalogo/catalogo.component.ts
@@ -7,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './catalogo.component.css'
 })
 export class CatalogoComponent {
-
+    
+  tienda = {
+    NombreTienda: 'StreetWear',
+    productos: [
+      {
+        id: 1,
+        nombre: 'Camiseta Básica',
+        descripcion: 'Camiseta de algodón 100% con diseño clásico.',
+        precio: '20.00€',
+        imagen: 'img/camiseta.webp'
+      },
+      {
+        id: 2,
+        nombre: 'Pantalón Chandal PSG',
+        descripcion: 'Confeccionado en materiales cómodos y de alta calidad',
+        precio: '35.00€',
+        imagen: 'img/panatalon.jpg'
+      },
+      {
+        id: 3,
+        nombre: 'Sudadera Nike Sportswear Club Crew',
+        descripcion: 'Su diseño clásico y suave tejido fleece la hacen perfecta para el uso diario.',
+        precio: '20.00€',
+        imagen: 'img/chaqueta.jpg'
+      },
+      {
+        id: 4,
+        nombre: 'Sudadera Oversized',
+        descripcion: 'Comodidad sin esfuerzo con un toque urbano.',
+        precio: '20.00€',
+        imagen: 'img/sudadera.jpg'
+      }
+    ]
+  }
 }
